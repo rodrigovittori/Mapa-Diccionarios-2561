a15f9f17b4f4e864592807c525d4ba584fbe903a
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-01 16:50:12 -0300

    # M4.L3 - Actividad #3: "Bucle Principal"
    Agregamos una variable que controla la habitación actual, un bucle de juego principal que lista las habitaciones accesibles desde la sala actual y finaliza al llegar a la salida

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# M4.L3 - Actividad #2: "Habitaciones" 
+import time
+
+# M4.L3 - Actividad #3: "Bucle Principal"
 
 mapa = {
         #       CLAVE       :               VALOR
@@ -11,3 +13,33 @@ mapa = {
         'Boss'              : ['4', 'Salida'],
         'Salida'            : ['Boss']
         }
+
+# Seteamos habitación inicial (Spawn)
+habitacion_actual = "Spawn"
+
+# Bucle de Juego:
+while(True): # To-do: agregar una condición para detener el bucle de juego
+  print('============================')
+  print('Estás en la habitación', habitacion_actual)
+
+  # Mostrar habitaciones disponibles/accesibles:
+  for habitacion_contigua in mapa[habitacion_actual]:
+    print("Puedes ir a", habitacion_contigua)
+
+  nueva_habitacion = input("¿Qué habitación eliges?: ")
+
+  #Validamos habitación destino:
+  if nueva_habitacion not in mapa[habitacion_actual]:
+    print("¡No puedes hacer eso!, ", nueva_habitacion, " NO es accesible desde aquí.")
+    time.sleep(2)
+    continue
+
+  elif (nueva_habitacion == "Salida"):
+    print("¡Eres libre!")
+    time.sleep(2)
+    break
+
+  else:
+    # Si la habitacion ES válida y NO es la salida:
+    habitacion_actual = nueva_habitacion
+    
