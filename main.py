217c0540c6ce4fd9185b421393a46b0bb0a180fe
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-01 18:33:08 -0300

    # M4.L3 - Actividad #7: "Tu laberinto (Adicional)"
    Modificamos el mapa y agregamos la mecánica de trampas

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import time
 
-# M4.L3 - Actividad #5: "Llave"
+# M4.L3 - Actividad #7: "Tu laberinto (Adicional)"
 
 mapa = {
         # 1ª CLAVE (del mapa) :    VALOR (otro diccionario)
         #                       2º diccionario (cada habitacion tiene su propio diccionario)
         #                       lista_habitaciones, lista_items, (pueden agregar: lista_trampas y/o lista_enemigos)
         
-        'Spawn'               : { 'lista_habitaciones' :  ['1', '2'], 'items' : [] },
-        '1'                   : { 'lista_habitaciones' :  ['Spawn', '3', '4'], 'items' : [] },
-        '2'                   : { 'lista_habitaciones' :  ['Spawn', '4'], 'items' : [] },
-        '3'                   : { 'lista_habitaciones' :  ['1'], 'items' : ['Planta misteriosa'] },
-        '4'                   : { 'lista_habitaciones' :  ['1', '2', 'Boss'], 'items' : [] },
-        'Boss'                : { 'lista_habitaciones' :  ['4', 'Salida'], 'items' : [] },
-        'Salida'              : { 'lista_habitaciones' :  ['Boss'], 'items' : [] }
+        'Dormitorio Invitados': { 'lista_habitaciones' :  ['Sala de Estar'],   'items' : [] , 'lista_trampas' : []},
+        'Sala de Estar'       : { 'lista_habitaciones' :  ['Baño', 'Cocina', 'Comedor', 'Dormitorio Invitados', 'Dormitorio Principal', 'Entrada'],
+                                                                               'items' : [] , 'lista_trampas' : []},
+        'Baño'                : { 'lista_habitaciones' :  ['Sala de Estar'],   'items' : [] , 'lista_trampas' : ['Trampa_Escopeta']},
+        'Dormitorio Principal': { 'lista_habitaciones' :  ['Sala de Estar'],   'items' : ['Llaves extra'], 'lista_trampas' : []}, # Posible tunel
+        'Comedor'             : { 'lista_habitaciones' :  ['Sala de Estar', 'Cocina'],  
+                                                                               'items' : [], 'lista_trampas' : []},
+        'Cocina'              : { 'lista_habitaciones' :  ['Comedor'],   'items' : ['Botella de veneno'], 'lista_trampas' : []},
+
+
+        'Entrada'             : { 'lista_habitaciones' :  ['Sala de Estar', 'Despacho', 'Salida'],
+                                                                               'items' : [], 'lista_trampas' : []},
+        
+        'Despacho'            : { 'lista_habitaciones' :  ['Sala de Estar'],   'items' : [], 'lista_trampas' : []},
+        'Salida'              : { 'lista_habitaciones' :  ['Entrada'],         'items' : [], 'lista_trampas' : []}
         }
+
 #  Habitación actual : Lista de habitaciones accesibles
 
 # Seteamos habitación inicial (Spawn)
-habitacion_actual = "Spawn"
-tiene_arma_legendaria = False
+habitacion_actual = "Dormitorio Invitados"
+tiene_llave = False
+intento_salir = False
+game_over = False
 
 # Bucle de Juego:
 while(True): # To-do: agregar una condición para detener el bucle de juego
@@ -41,32 +52,40 @@ while(True): # To-do: agregar una condición para detener el bucle de juego
   else:
     
     # Si la habitacion ES válida
-    if (nueva_habitacion == "Salida"):
-      print("¡Eres libre!")
+    if (nueva_habitacion == "Entrada") and tiene_llave:
+      print("Te aproximas a la puerta  tan rápido como puedes mientras te aseguras de que no haya ninguna trampa más y usas la llave.")
+      time.sleep(2)
+      print("La ansiedad dificulta que tus temblorosas manos logren introducir la pequeña llave en la intrincada cerradura sin causar mayor ruido...")
+      time.sleep(2)
+      print("Casi sin respirar giras la llave, accionando el mecanismo, cada segundo pesando como una eternidad mientras tus pulmones arden cy tu cabeza a punto de estallar")
+      print(" Funciona, ¡ Finalmente eres libre!")
+      # To-do agregar texto con condiciones para saber si tenemos pruebas de los crímenes / tesoro / si des-vivimos al asesino, etc...
       time.sleep(2)
       break
 
-    elif (nueva_habitacion == "Boss") and not tiene_arma_legendaria:
-      print("La presencia del malo malvado es tan maléficamente malvada que tus piernas se vuelven blandas como malvaviscos y no puedes obligarte a entrar.")
-      print("AUN NO ESTAS LISTO, ¡ENCUENTRA EL ARMA LEGENDARIA QUE LO DERROTARÁ!")
+    elif ((nueva_habitacion == "Entrada") and (not tiene_llave) and (not intento_salir)):
+      # To-do: agregar drama
+      print("Ves la puerta, la probás, tiene llave, vos no tenés la llave. Raios.")
+      intento_salir = True
       time.sleep(2)
 
-    elif (nueva_habitacion == "Boss") and tiene_arma_legendaria:
-      print("El malo malvado comienza un laaaaaaaaargo discurso sobre como unos aventureros como ustedes nunca podrán con un mago tan poderoso como él que ha aterrorizado estas tierras por más de 500 años.")
-      print("-\"Y es EXACTAMENTE lo que nuestro amiguito estaba buscando\" :)")
-      print("-\"¡VÉ JUAN!, ¡ATRÁPALO!\"")
-      print("** Juan es una iguana devoradora de maná **")
-      print("La escencia vital del mago malvado es absorbida en un instante por Juan, quien crece descomunalmente, empjándote hacia la Salida...")
-      print("No sólo has salvado al reino, sino que has hecho muy feliz a tu nuevo (y ya no tan pequeño) amigo: JUAN")
-      print("\n GOOD ENDING: \" My friend JUAN\" ")
+    elif ((nueva_habitacion == "Entrada") and (not tiene_llave) and intento_salir):
+      print("Una vez más te encuentras frente a la puerta, más sabes que necesitas la llave para escapar.")
       time.sleep(2)
 
     habitacion_actual = nueva_habitacion
 
     # Chequear items
-    if "Planta misteriosa" in mapa[habitacion_actual]['items']:
-      tiene_arma_legendaria = True
-      print("En la habitación encuentras una pequeña planta misteriosa que llama tu atención, al acercarte se enrieda en tu antebrazo y no logras despegarla de tí")
-      mapa[habitacion_actual]['items'].remove('Planta misteriosa')
+    if "Llaves extra" in mapa[habitacion_actual]['items']:
+      tiene_llave = True
+      print("En la habitación encuentras un juego de llaves extra")
+      mapa[habitacion_actual]['items'].remove('Llaves extra')
       time.sleep(2)
       continue
+
+    if "Trampa_Escopeta" in mapa[habitacion_actual]['lista_trampas']:
+      game_over = True
+      print("Al entrar a la habitación {drama drama drama}, una escopeta te deja con el último look de Kurt Cobain")
+      mapa[habitacion_actual]['lista_trampas'].remove('Trampa_Escopeta')
+      time.sleep(2)
+      break
